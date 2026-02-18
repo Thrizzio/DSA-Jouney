4f173375e9d626060ddc219648749a1fa5efeeb8
Aadi Jain <aadikseth@gmail.com>
2026-02-18 23:22:42 +0530

    Solved Binary Search Questions like 33 and 153

--- greedy/jump_game.java
+++ greedy/jump_game.java
@@ -1,3 +1,6 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
 class Solution {
     public boolean canJump(int[] nums) {
         int maxTotal=0;
@@ -12,3 +15,18 @@ class Solution {
         
     }
 }
+
+/*
+Problem : 55
+Platform : LeetCode
+Pattern : Greedy
+Difficulty : Hard
+
+Initial Thoughts: To try every single combination of every single possible route, could not implement and unecessary computation usage.
+
+Intuition : Initialize a variable to see what the maxIndex you can go to is, but , if the index you are at right now is greater than the maxIndex it can go to till then , then , return false, but , return true if the maxIndex >= n-1 or if entire array is traversed.
+
+TC : O(n)
+SC = O(1)
+
+*/
--- two pointers/bag_of_tokens.cpp
+++ two pointers/bag_of_tokens.cpp
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -36,3 +29,17 @@ public:
         return maxScore;
     }
 };
+
+/*
+Problem : 948
+Platform : LeetCode
+Pattern : Greedy + Two Pointers
+
+Initial Idea: It was to gain score everytime my power was less than token , but that is very illogical
+
+Intuition: You gain the max power possible by keeping a right pointer at the max token and therefore you are being Greedy by adding the maxPower to your power , making it so that it is the best option you can have.
+
+TC = O(nlogn);
+SC = O(1);
+
+*/
